a5fcd547483de32b1b8749930f72dad269d6aab2
Jandy Stephens <36689860+JandyStephens@users.noreply.github.com>
2022-06-30 01:41:12 -0700

    added name field and started setting State to save player name

--- src/Components/SplashPage/index.jsx
+++ src/Components/SplashPage/index.jsx
@@ -5,8 +5,24 @@ import CustomButton from "../CustomButton";
 const SplashPage = (props) => {
   return (
     <div>
-      <HeaderText className="splashPage">Trivia!</HeaderText>
-      <p>Instructions here</p>
+      <HeaderText className="splashPage">Trivia</HeaderText>
+      <p>
+        Select the correct answer(s) to the following 5 questions. Your final
+        score will be displayed at the end. Enjoy!
+      </p>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <br />
+        <input
+          name="playerName"
+          type="text"
+          placeholder="Enter your name here"
+        />
+      </form>
+      <br />
       <CustomButton nextPage="/questionOne">Let's Begin!</CustomButton>
     </div>
   );
---
+++ src/Components/StoreFieldInput/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useState } from "react";
+
+export default StoreFieldInput = (props) => {
+  const [name, setName] = useState([]);
+  return alert("temp expression");
+};
