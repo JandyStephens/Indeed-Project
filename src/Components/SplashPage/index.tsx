60c7691cb2c08d64855aee5a1bd6b5ac63b18758
Jandy Stephens <36689860+JandyStephens@users.noreply.github.com>
2022-07-17 14:03:55 -0400

    added commenting for wow button

--- src/Components/SplashPage/index.tsx
+++ src/Components/SplashPage/index.tsx
@@ -4,6 +4,8 @@ import CustomButton from "../CustomButton";
 import TriviaBanner from "../../assets/images/trivia_banner.jpeg";
 import NeonTriviaBanner from "../../assets/images/trivia_neon.jpeg";
 
+//This button was made in the spirit of fun for the 'wow' factor. Labelling was intentionally left off because I wanted curiousity of the user about "that weird blinking button" to be engaged. Because of this, I understand that it is not completely accessible to everyone. Overall, I made sure that the main requirements were accessible and if someone one did not see it or cannot see it, it would not impact their overall experience.
+
 const SplashPage = (props) => {
   return (
     <div className="container">
